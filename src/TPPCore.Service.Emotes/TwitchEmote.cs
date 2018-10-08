6650837f6f96c53c205ecac1d46981dad84ef11d
red031000 <rubenru09@aol.com>
2018-10-08 20:03:18 +0100

    reduce the amount of memory taken up by the Emote Service

--- src/TPPCore.Service.Emotes/TwitchEmote.cs
+++ src/TPPCore.Service.Emotes/TwitchEmote.cs
@@ -1,14 +1,12 @@
-﻿using System.Collections.Generic;
-
-namespace TPPCore.Service.Emotes
+﻿namespace TPPCore.Service.Emotes
 {
-    public class TwitchEmote : EmoteInfo
+    internal class TwitchEmote : EmoteInfo
     {
         public TwitchEmote(int id, string code)
         {
             Id = id;
             Code = code;
-            ImageUrls = new List<string> { $"https://static-cdn.jtvnw.net/emoticons/v1/{id}/1.0", $"https://static-cdn.jtvnw.net/emoticons/v1/{id}/2.0", $"https://static-cdn.jtvnw.net/emoticons/v1/{id}/3.0" };
         }
+        public override string[] ImageUrls => new string[] { $"https://static-cdn.jtvnw.net/emoticons/v1/{Id}/1.0", $"https://static-cdn.jtvnw.net/emoticons/v1/{Id}/2.0", $"https://static-cdn.jtvnw.net/emoticons/v1/{Id}/3.0" };
     }
 }
